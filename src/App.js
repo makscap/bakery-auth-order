3d15847ec2e9f20c740d30a96552f423d0dac132
makscap <63607294+makscap@users.noreply.github.com>
2022-05-23 18:33:12 +0200

    added btn for post

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import CardBox from "./components/Card/Card";
 import logo from "./images/bread.png";
 import TableOfResults from "./components/TableOfResults/TableOfResults";
 import React, { useState } from "react";
+import Button from "@mui/material/Button";
 
 function App() {
   const [state, setState] = useState([]);
@@ -10,18 +11,43 @@ function App() {
   const [quantity, setQuantity] = useState(0);
   const [totalQuantity, setTotalQuantity] = useState(0);
 
+  const showApi = () => {
+    return console.log(state);
+  };
+
   return (
     <div className="App">
       <header
         style={{
           display: "flex",
           alignItems: "center",
+          justifyContent: "space-between",
           height: "50px",
           borderBottom: "1px solid darkgrey",
         }}
       >
-        <img src={logo} alt="logo" width="40px" />
-        <span style={{ fontWeight: "700" }}>BREAD</span>
+        <div>
+          <a
+            href="/"
+            style={{
+              display: "flex",
+              alignItems: "center",
+              height: "50px",
+              borderBottom: "1px solid darkgrey",
+              marginLeft: "10px",
+              textDecoration: "none",
+              color: "black",
+            }}
+          >
+            <img src={logo} alt="logo" width="40px" />
+            <span style={{ fontWeight: "700", marginLeft: "10px" }}>BREAD</span>
+          </a>
+        </div>
+        <div style={{ marginRight: "10px" }}>
+          <Button variant="outlined" onClick={showApi}>
+            POST (console!)
+          </Button>
+        </div>
       </header>
       <div style={{ display: "flex", justifyContent: "center" }}>
         <CardBox
